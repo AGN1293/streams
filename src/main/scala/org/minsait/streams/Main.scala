ffda60482028c455df8b0e2ae106db5cc75a1abd
Adrian <agn1293@gmail.com>
2019-06-06 11:17:00 +0200

    Deleted offset reset

--- src/main/scala/org/minsait/streams/Main.scala
+++ src/main/scala/org/minsait/streams/Main.scala
@@ -23,7 +23,6 @@ object Main extends App {
     p.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
     p.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String.getClass)
     p.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass)
-    p.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
     p
   }
 
