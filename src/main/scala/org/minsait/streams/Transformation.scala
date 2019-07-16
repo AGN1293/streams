e537db194bcb4bd5878f4b8b01632a97b6f76b8c
Gisber Navarro, Adrian <agisbert@eservicios.indra.es>
2019-07-16 09:05:03 +0200

    Changednull

--- src/main/scala/org/minsait/streams/Transformation.scala
+++ src/main/scala/org/minsait/streams/Transformation.scala
@@ -58,63 +58,65 @@ object Transformation {
         val tenantId = msg.after.TENANT_ID
         var xIndex = -1
         val currentDate = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSS").format(new Date())
-        msg.after.FORMINSTANCEFIELDS.get.foreach {
-          x =>
-            xIndex += 1
-            var yIndex = -1
-            x.FieldsList.foreach {
-              y =>
-                yIndex += 1
-                val businessConcept = y.BusinessConceptId.getOrElse(0)
-                y.DFormFieldTypeId match {
-                  case 3 =>
-                    if (y.IsFilled.getOrElse(false)) {
-                      results += dummyJson.copy(payload = Some(ResponseMessage(id, tenantId, xIndex + "-" + yIndex + "-0", 3, businessConcept, y.Label, y.TextBoxField.get.Value.get, msg.op_type, msg.op_ts, currentDate, msg.pos)))
-                    }
-                  case 4 =>
-                    if (y.DatetimeFieldId.isDefined && y.DatetimeFieldId.get.Value.isDefined) {
-                      results += dummyJson.copy(payload = Some(ResponseMessage(id, tenantId, xIndex + "-" + yIndex + "-0", 4, businessConcept, y.Label, y.DatetimeFieldId.get.Value.get, msg.op_type, msg.op_ts, currentDate, msg.pos)))
-                    }
-                  case 5 =>
-                    if (y.IsFilled.getOrElse(false) && y.LogicFieldId.isDefined) {
-                      if (y.LogicFieldId.get.Value.getOrElse(false))
-                        results += dummyJson.copy(payload = Some(ResponseMessage(id, tenantId, xIndex + "-" + yIndex + "-0", 5, businessConcept, y.Label, y.LogicFieldId.get.LabelTrue.getOrElse(""), msg.op_type, msg.op_ts, currentDate, msg.pos)))
-                      else
-                        results += dummyJson.copy(payload = Some(ResponseMessage(id, tenantId, xIndex + "-" + yIndex + "-0", 5, businessConcept, y.Label, y.LogicFieldId.get.LabelFalse.getOrElse(""), msg.op_type, msg.op_ts, currentDate, msg.pos)))
-                    }
-                  case 6 =>
-                    if (y.OptionListFieldId.isDefined && y.OptionListFieldId.get.OptionChoicesList.isDefined) {
-                      val optionList = y.OptionListFieldId.get.OptionChoicesList.get
-                      var zIndex = -1
-                      optionList.foreach {
-                        option =>
-                          zIndex += 1
-                          if (option.IsSelected.isDefined && option.IsSelected.get && option.Name.isDefined)
-                            results += dummyJson.copy(payload = Some(ResponseMessage(id, tenantId, xIndex + "-" + yIndex + "-" + zIndex, 6, businessConcept, y.Label, option.Name.get, msg.op_type, msg.op_ts, currentDate, msg.pos)))
+        if (msg.after.FORMINSTANCEFIELDS.isDefined) {
+          msg.after.FORMINSTANCEFIELDS.get.foreach {
+            x =>
+              xIndex += 1
+              var yIndex = -1
+              x.FieldsList.foreach {
+                y =>
+                  yIndex += 1
+                  val businessConcept = y.BusinessConceptId.getOrElse(0)
+                  y.DFormFieldTypeId match {
+                    case 3 =>
+                      if (y.IsFilled.getOrElse(false)) {
+                        results += dummyJson.copy(payload = Some(ResponseMessage(id, tenantId, xIndex + "-" + yIndex + "-0", 3, businessConcept, y.Label, y.TextBoxField.get.Value.get, msg.op_type, msg.op_ts, currentDate, msg.pos)))
                       }
-                    }
-                  case 7 =>
-                    if (y.NumericFieldId.isDefined && y.NumericFieldId.get.Value.isDefined)
-                      results += dummyJson.copy(payload = Some(ResponseMessage(id, tenantId, xIndex + "-" + yIndex + "-0", 7, businessConcept, y.Label, y.NumericFieldId.get.Value.get.toString, msg.op_type, msg.op_ts, currentDate, msg.pos)))
-                  case 8 =>
-                    if (y.AttachmentFieldId.isDefined && y.AttachmentFieldId.get.AttachmentLines.isDefined && y.AttachmentFieldId.get.AttachmentLines.get.AttachmentLineFieldList.isDefined && y.AttachmentFieldId.get.AttachmentLines.get.AttachmentLineFieldList.get.nonEmpty) {
-                      val value = y.AttachmentFieldId.get.AttachmentLines.get.AttachmentLineFieldList.get.head.Value.getOrElse("")
-                      val fileName = y.AttachmentFieldId.get.AttachmentLines.get.AttachmentLineFieldList.get.head.FileName.get
-                      results += dummyJson.copy(payload = Some(ResponseMessage(id, tenantId, xIndex + "-" + yIndex + "-0", 8, businessConcept, y.Label, value + ":" + fileName, msg.op_type, msg.op_ts, currentDate, msg.pos)))
-                    }
-                  case 9 =>
-                    if (y.OptionListFieldId.isDefined && y.OptionListFieldId.get.OptionChoicesList.isDefined) {
-                      var zIndex = -1
-                      y.OptionListFieldId.get.OptionChoicesList.get.foreach {
-                        option =>
-                          zIndex += 1
-                          if (option.IsSelected.getOrElse(false))
-                            results += dummyJson.copy(payload = Some(ResponseMessage(id, tenantId, xIndex + "-" + yIndex + "-" + zIndex, 9, businessConcept, y.Label, option.Name.get, msg.op_type, msg.op_ts, currentDate, msg.pos)))
+                    case 4 =>
+                      if (y.DatetimeFieldId.isDefined && y.DatetimeFieldId.get.Value.isDefined) {
+                        results += dummyJson.copy(payload = Some(ResponseMessage(id, tenantId, xIndex + "-" + yIndex + "-0", 4, businessConcept, y.Label, y.DatetimeFieldId.get.Value.get, msg.op_type, msg.op_ts, currentDate, msg.pos)))
                       }
-                    }
-                  case _ =>
-                }
-            }
+                    case 5 =>
+                      if (y.IsFilled.getOrElse(false) && y.LogicFieldId.isDefined) {
+                        if (y.LogicFieldId.get.Value.getOrElse(false))
+                          results += dummyJson.copy(payload = Some(ResponseMessage(id, tenantId, xIndex + "-" + yIndex + "-0", 5, businessConcept, y.Label, y.LogicFieldId.get.LabelTrue.getOrElse(""), msg.op_type, msg.op_ts, currentDate, msg.pos)))
+                        else
+                          results += dummyJson.copy(payload = Some(ResponseMessage(id, tenantId, xIndex + "-" + yIndex + "-0", 5, businessConcept, y.Label, y.LogicFieldId.get.LabelFalse.getOrElse(""), msg.op_type, msg.op_ts, currentDate, msg.pos)))
+                      }
+                    case 6 =>
+                      if (y.OptionListFieldId.isDefined && y.OptionListFieldId.get.OptionChoicesList.isDefined) {
+                        val optionList = y.OptionListFieldId.get.OptionChoicesList.get
+                        var zIndex = -1
+                        optionList.foreach {
+                          option =>
+                            zIndex += 1
+                            if (option.IsSelected.isDefined && option.IsSelected.get && option.Name.isDefined)
+                              results += dummyJson.copy(payload = Some(ResponseMessage(id, tenantId, xIndex + "-" + yIndex + "-" + zIndex, 6, businessConcept, y.Label, option.Name.get, msg.op_type, msg.op_ts, currentDate, msg.pos)))
+                        }
+                      }
+                    case 7 =>
+                      if (y.NumericFieldId.isDefined && y.NumericFieldId.get.Value.isDefined)
+                        results += dummyJson.copy(payload = Some(ResponseMessage(id, tenantId, xIndex + "-" + yIndex + "-0", 7, businessConcept, y.Label, y.NumericFieldId.get.Value.get.toString, msg.op_type, msg.op_ts, currentDate, msg.pos)))
+                    case 8 =>
+                      if (y.AttachmentFieldId.isDefined && y.AttachmentFieldId.get.AttachmentLines.isDefined && y.AttachmentFieldId.get.AttachmentLines.get.AttachmentLineFieldList.isDefined && y.AttachmentFieldId.get.AttachmentLines.get.AttachmentLineFieldList.get.nonEmpty) {
+                        val value = y.AttachmentFieldId.get.AttachmentLines.get.AttachmentLineFieldList.get.head.Value.getOrElse("")
+                        val fileName = y.AttachmentFieldId.get.AttachmentLines.get.AttachmentLineFieldList.get.head.FileName.get
+                        results += dummyJson.copy(payload = Some(ResponseMessage(id, tenantId, xIndex + "-" + yIndex + "-0", 8, businessConcept, y.Label, value + ":" + fileName, msg.op_type, msg.op_ts, currentDate, msg.pos)))
+                      }
+                    case 9 =>
+                      if (y.OptionListFieldId.isDefined && y.OptionListFieldId.get.OptionChoicesList.isDefined) {
+                        var zIndex = -1
+                        y.OptionListFieldId.get.OptionChoicesList.get.foreach {
+                          option =>
+                            zIndex += 1
+                            if (option.IsSelected.getOrElse(false))
+                              results += dummyJson.copy(payload = Some(ResponseMessage(id, tenantId, xIndex + "-" + yIndex + "-" + zIndex, 9, businessConcept, y.Label, option.Name.get, msg.op_type, msg.op_ts, currentDate, msg.pos)))
+                        }
+                      }
+                    case _ =>
+                  }
+              }
+          }
         }
         results.toList.map(event => event.asJson.pretty(printer))
       }
