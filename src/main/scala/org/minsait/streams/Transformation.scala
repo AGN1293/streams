39297f8b3ecbd5d741c66202bb67625b86280072
Gisbert Navarro <agisbert@eservicios.indra.es>
2019-08-28 15:11:19 +0200

    d

--- src/main/scala/org/minsait/streams/Transformation.scala
+++ src/main/scala/org/minsait/streams/Transformation.scala
@@ -24,7 +24,7 @@ object Transformation {
   var results: ArrayBuffer[JsonResponse] = ArrayBuffer.empty
 
   def jsonToClass(input: String): Option[JsonMessage] = {
-    val inputFormatted = input.replace("\\\\\\\"", "").replace("\"[", "[").replace("]\"", "]").replace("\\", "")
+    val inputFormatted = input.replace("\\\\\\\"", "").replace("\"[", "[").replace("]\"", "]").replace("\\", "").replaceAll("[^\u0000-\uFFFF]", "")
     val decoded = decode[JsonMessage](inputFormatted)
     decoded match {
       case Right(msg) => Some(msg)
